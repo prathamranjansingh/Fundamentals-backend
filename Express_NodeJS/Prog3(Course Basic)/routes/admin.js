a5a58f0fd678b4afe510f6373f72995ae55ea3c8
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-06-13 20:05:59 +0530

    Prog3 completed Prog4 Jwt started

--- Express_NodeJS/Prog3(Course Basic)/routes/admin.js
+++ Express_NodeJS/Prog3(Course Basic)/routes/admin.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const express = require("express");
 const adminMiddleware = require("../middleware/admin");
-const { Admin } = require("../db");
+const { Admin, Course } = require("../db");
 const router = express.Router();
 
 // Admin Routes (/admin/signup) quki main index.js mein admin krke call kiya hai is router ko
@@ -21,12 +21,39 @@ router.post("/signup", async (req, res) => {
   });
 });
 
-router.post("/courses", adminMiddleware, (req, res) => {
+router.post("/courses", adminMiddleware, async (req, res) => {
   // Implement course creation logic
+  const title = req.body.title;
+  const description = req.body.description;
+  const imageLink = req.body.imageLink;
+  const price = req.body.price;
+
+  const newCourse = await Course.create({
+    title,
+    description,
+    imageLink,
+    price,
+  });
+  res.json({
+    msg: "Course created successfully",
+    courseId: newCourse._id,
+  });
 });
 
-router.get("/courses", adminMiddleware, (req, res) => {
+router.get("/courses", adminMiddleware, async (req, res) => {
   // Implement fetching all courses logic
+  //this is the same logic but with a promise so if you want to use this just remove the async from the res res
+  // Course.find({}).then(function (response) {
+  //   res.json({
+  //     courses: response,
+  //   });
+  // });
+
+  const response = await Course.find({});
+
+  res.json({
+    courses: response,
+  });
 });
 
 module.exports = router;
--- Express_NodeJS/Prog3(Course Basic)/routes/user.js
+++ Express_NodeJS/Prog3(Course Basic)/routes/user.js
@@ -1,21 +1,70 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
 router.post("/signup", (req, res) => {
   // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  User.create({
+    username,
+    password,
+  });
+  res.json({
+    msg: "User created successfully",
+  });
 });
 
-router.get("/courses", (req, res) => {
+router.get("/courses", async (req, res) => {
   // Implement listing all courses logic
+  const response = await Course.find({});
+
+  res.json({
+    courses: response,
+  });
 });
 
-router.post("/courses/:courseId", userMiddleware, (req, res) => {
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
   // Implement course purchase logic
+  //we could have simply created a new table of Purchase which could have just the to data, the userId and the courses they bought CourseId
+  // so in that we would just have to create it by Purchase.create({userId = userId, courseId = courseId})
+  //but we took a diff route so database has 3 tables which has references PurchasesCourse
+
+  const courseId = req.params.courseId;
+  const username = req.headers.username;
+
+  await User.updateOne(
+    {
+      username: username,
+    },
+    {
+      $push: {
+        purchasedCourses: courseId,
+      },
+    }
+  );
+  res.json({
+    message: "Purchase complete!",
+  });
 });
 
-router.get("/purchasedCourses", userMiddleware, (req, res) => {
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
   // Implement fetching purchased courses logic
+  const user = await User.findOne({
+    username: req.headers.username,
+  });
+
+  console.log(user.purchasedCourses);
+  const courses = await Course.find({
+    _id: {
+      $in: user.purchasedCourses,
+    },
+  });
+
+  res.json({
+    courses: courses,
+  });
 });
 module.exports = router;
