a0f858165ce069cbbb62bf9c05bf6b7986b06f41
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-06-13 21:31:39 +0530

    Prog4 completed

---
+++ Express_NodeJS/Prog4 (jwt mongodb)/config.js
@@ -0,0 +1,1 @@
+module.exports = { JWT_SECRET: "Pratham_server_this_can_be_anything" };
--- Express_NodeJS/Prog4 (jwt mongodb)/middleware/admin.js
+++ Express_NodeJS/Prog4 (jwt mongodb)/middleware/admin.js
@@ -1,7 +1,28 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
   // Implement admin auth logic
   // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  const token = req.headers.authorization;
+  // token looks like something - Bearer adadahuisfafh23u4hiu1iu
+  const words = token.split(" "); //this will convert the token to array ['bearer', 'sdasasfbsgius']
+  const jwtToken = words[1];
+  const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+  try {
+    const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+    if (decodedValue.username) {
+      next();
+    } else {
+      res.status(403).json({
+        msg: "You are not authenticated",
+      });
+    }
+  } catch (e) {
+    res.json({
+      msg: "Incorrect inputs",
+    });
+  }
 }
 
 module.exports = adminMiddleware;
